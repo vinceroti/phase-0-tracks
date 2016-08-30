18ee0083826f4a576e18646adab0a934246b4a78
Vince Roti <vince.roti@gmail.com>
2016-08-29 21:19:00 -0500

    finished release 5

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,55 +1,56 @@
-# Gathering Data
+# var, bools, and other things
 current_year = 2016
-
-p "Werewolf Inc Application Form"
-
-
-p "How many employees will be processed?"
-employees = gets.chomp.to_i
+done = false #bool for while loop
+sunshine = false #bool for sunshine, if true vampire they are!
 counter = 0 #counter for while loop in order to process multiple
 #employees
 
-while counter <= employees
+puts "Werewolf Inc Application Form"
+
+
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
+
+#employee counter loop
+
+until counter >= employees
   counter += 1
 
-  p"Application for employee number #{counter}"  
+  puts "Application for employee number #{counter}"  
    
-  p "What is your name?"
+  puts  "What is your name?"
   name = gets.chomp
 
-  p "How old are you?"
+  puts "How old are you?"
   age = gets.chomp.to_i
 
-  p "What year were you born?"
+  puts "What year were you born?"
   year = gets.chomp.to_i
 
-  p "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
   bread = gets.chomp
 
   #conditional for bread to ensure yes or no answer
 
   while bread != 'yes' && bread != 'no'
-    p "Please input 'yes' or 'no' only"
+    puts "Please input 'yes' or 'no' only"
     bread = gets.chomp
   end
 
-  p "Would you like to enroll in the company's health insurance? (yes/no)"
+  puts "Would you like to enroll in the company's health insurance? (yes/no)"
   insurance = gets.chomp
 
   #conditional for insurance to ensure yes or no answer
 
   while insurance != 'yes' && insurance != 'no'
-    p "Please input 'yes' or 'no' only"
+    puts "Please input 'yes' or 'no' only"
     insurance = gets.chomp
   end
 
 #while loop for allergies and sunshine checker
 
-  p "Name any allergies(one at a time)"
-  p "Enter 'Done' when finished."
-
-  done = false #bool for while loop
-  sunshine = false #bool for sunshine, if true vampire they are!
+  puts "Name any allergies(one at a time)"
+  puts "Enter 'Done' when finished."
 
   while done == false
       allergies = gets.chomp
@@ -62,16 +63,21 @@ while counter <= employees
   end
 
   if name == 'Drake Cula' || name == 'Tu Fang'
-    p "Definitely a vampire."
+    puts "Definitely a vampire."
     elsif age + year != current_year && bread == 'no' && insurance == 'no'
-    p "Almost certainly a vampire."
+    puts "Almost certainly a vampire."
     elsif (sunshine == true) || age + year != current_year && bread == 'no' || insurance == 'no'
-    p "Probably a vampire."
+    puts "Probably a vampire."
     elsif age + year == current_year && bread == 'yes' || insurance == 'yes'
-      p "Probably not a vampire."
+      puts "Probably not a vampire."
     else
-      p "Results inconclusive."
+      puts "Results inconclusive."
   end #ends if statement
 
-end #ends while loop for employees
+    done = false #resets bool for next employee
+    sunshine = false #resets bool for next employee
+
+end #ends until loop for employees
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
