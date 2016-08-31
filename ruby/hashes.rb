437b53548e35597f9b35bc970f8de7e1b0f6f730
Vince Roti <vince.roti@gmail.com>
2016-08-30 21:20:00 -0500

    working driver and debugger

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,3 +6,56 @@
 #Allow user to update info after they have entered their info, if no updating skip over
 #Print out all info over for user to see.
 
+#hash for client's information 
+
+client = {
+  name: "",
+  address: "",
+  age: 0,
+  email: "",
+  phone: 0,
+  children: false,
+  decor_theme: "",
+}
+
+#driver code for user input
+
+puts "Welcome to the Interior Designer Database"
+puts "Please enter your client's information"
+
+puts "Name"
+client[:name] = gets.chomp
+
+puts "Age" 
+client[:age] = gets.chomp.to_i
+
+puts "Address"
+client[:address] = gets.chomp
+
+puts "Email"
+client[:email] = gets.chomp
+
+puts "Phone"
+client[:phone] = gets.chomp.to_i
+
+puts "Are there children? (Yes or No)"
+client[:children] = gets.chomp.downcase
+
+#ensuring yes or no answer code
+until client[:children] == "yes" || client[:children] == "no"
+  puts "'Yes' or 'No' only"
+  client[:children] = gets.chomp.downcase
+end
+
+#checks input of child input
+if client[:children] == 'yes'
+  client[:children] = true
+else
+  client[:children] = false
+end
+
+#debugging
+
+p client
+
+
