a65224377c3748b2648af77139664598a54effb4
Vince Roti <vince.roti@gmail.com>
2016-08-31 14:43:59 -0500

    finished release 1 with .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+#release 0
+
 def greeting
   name1 = "Will"
   name2 = "Tom" 
@@ -8,3 +10,41 @@ end
 
 greeting { |name1, name2| puts "What's up, #{name1} and #{name2}." }
 
+#release 1
+
+fruit = ["apples", "strawberries", "bananas"]
+spoiled = []
+
+video_games = { "Halo" => "good", "Overwatch" => "great", "Call Of Duty" => "alright"}
+
+p fruit
+p spoiled
+
+#modified with shovel operator
+
+fruit.each do |food|
+  puts "#{food} are pretty tasty!"
+  food += " are spoiled"
+  spoiled << food
+  puts "but now its spoiled :("
+end
+
+p fruit
+p spoiled
+
+#modified with .map! instead
+
+  fruit.map! do |food|
+  puts "#{food} are pretty tasty!"
+  puts "but now its spoiled :("
+  food += " are spoiled"
+end
+
+p fruit
+
+video_games.each do |game, rating|
+  puts "#{game}, we think is #{rating}."
+end
+
+
+#release 2
