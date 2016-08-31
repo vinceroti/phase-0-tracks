747917500eac889b7ee7d10ee9b24deacdb85f94
Vince Roti <vince.roti@gmail.com>
2016-08-30 20:39:27 -0500

    pseudocode added to top of document

---
+++ ruby/hashes.rb
@@ -0,0 +1,8 @@
+#This is a program that will allow user input to enter the following
+#The client's name, age, number of children, decor theme, etc
+#We must prompt user to enter their data
+#Convert input to right data type
+#print out hash info after client has entered their data
+#Allow user to update info after they have entered their info, if no updating skip over
+#Print out all info over for user to see.
+
