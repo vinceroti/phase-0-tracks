90f3091da05bd1ada03d909a9d6990820dc5b536
Vince Roti <vince.roti@gmail.com>
2016-08-31 21:56:48 -0500

    added hash data and nested data

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,41 @@
+#hash of data with arrays
+
+Mountains = {
+  copper: {
+    elevation: 12313,
+    lifts: {
+      speed: "fast",
+      amount: 23,
+    },
+    user_reviews: [
+      "Love the lay out of the runs",
+      "Beautiful area"
+      "Great value"
+    ],
+  }
+  breckenridge: {
+    elevation: 12998,
+    lifts: {
+      speed: "fast",
+      amount: 34,
+    },
+    user_reviews: [ 
+      "Great Snow", 
+      "Town is Awesome", 
+      "Daily lift ticket is way overpriced",
+    ]
+  }
+
+  keystone: {
+    elevation: 12408,
+    lifts: {
+      speed: "fast",
+      amount: 20,
+    },
+    user_reviews: [
+      "Favorite place to board in Colorado!",
+      "Family friendly.",
+      "Daily pass is to expansive",
+    ],
+  }
+}
