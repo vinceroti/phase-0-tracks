d1b374d4d4c2f2d73c5456dd3e70bbee597b2bdc
Vince Roti <vince.roti@gmail.com>
2016-09-01 23:42:28 -0500

    working name scrambler

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,33 +7,80 @@
 #Put it into an array split it and reverse it so the last name is first
 #Split the array even further or make two different arrays for first and last
 #Change all the vowels to the next vowel then do the same consonants
+#bring arrays together after splitter and show user their new words
 
 def name_spliter(full_name)
+  $full_name = gets.chomp.downcase
+  $full_name = $full_name.split
 
-  first = full_name.first
-  last = full_name.delete_at(1)
+  #takes first index of array moves it to new array and deletes it
+  first = $full_name.first
+  last = $full_name.delete_at(1)
   first = first.split('')
   last = last.split('')
+  #splits the array in order to access all of the data
+  #new array for iteration
+  scrambled_first = []
+  scrambled_last = []
 
-  p first
-  p last
 
-  #first.map! { |aeiou| aeiou =~ /[aeiou]/}
-  last.map! { |letter| letter.next } 
+  #Iterates through array for first name in order to process AEIOU and Special Case Z
+ first.each do |x| 
+    if x == "a"
+      x = "e"
+      elsif x == "e"
+        x = "i"
+      elsif x == "i"
+        x = "o"
+      elsif x == "o"
+        x = "u"
+      elsif x == "u"
+        x = "a"
+      elsif x == "z"
+        x = "a"
+      else 
+        x = x.next 
+    end
+  scrambled_first << x 
+  end
 
- # first.select { |aeiou| aeiou}
+  #Iterates through array for last name in order to process AEIOU and Special Case Z
+   last.each do |x| 
+    if x == "a"
+      x = "e"
+      elsif x == "e"
+        x = "i"
+      elsif x == "i"
+        x = "o"
+      elsif x == "o"
+        x = "u"
+      elsif x == "u"
+        x = "a"
+      elsif x == "z"
+        x = "a"
+      else 
+        x = x.next 
+    end
+  scrambled_last << x 
+  end
 
-   p first
-   p last
+  $full_name = scrambled_last.join('').capitalize + " " + scrambled_first.join('').capitalize
 end
 
+def data(full_name)
+  names = {}
+  names[:name] = $full_name 
+end
 #driver
 puts "Hello and welcome to the 2016 Alias Generator®"
 
 puts "Please enter your name:"
-full_name = gets.chomp.split
+puts "Type 'Quit' when finished"
 
-name_spliter(full_name)
+name_spliter($full_name)
+data($full_name)
 
-puts "Thank you! You're new name is #{full_name}"
+puts "Thank you! You're new name is #{$full_name}"
+
+
 puts "Goodbye."
