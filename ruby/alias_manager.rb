a2702e5f50a87acbcbde9416033aa93bceb5f3e3
Vince Roti <vince.roti@gmail.com>
2016-09-01 21:15:53 -0500

    created pseudocode and driver

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,19 @@
+#Name Scrambler Application
+
+#Pseudocode
+
+#First we must collect the name
+#Make it lower case
+#Put it into an array split it and reverse it so the last name is first
+#Split the array even further or make two different arrays for first and last
+#Change all the vowels to the next vowel then do the same consonants
+
+
+#driver
+puts "Hello and welcome to the 2016 Alias Generator®"
+
+puts "Please enter your name:"
+name = gets.chomp
+
+Puts "Thank you! You're new name is #{name}"
+Puts "Goodbye."
