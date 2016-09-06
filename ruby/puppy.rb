7c68bfbef4ad7355ec4a88f28f7cf0af3e02f0b8
Vince Roti <vince.roti@gmail.com>
2016-09-06 15:13:04 -0500

    puppy file with class comments and review file made

---
+++ ruby/puppy.rb
@@ -0,0 +1,23 @@
+# Classes
+
+# Puppy Class
+
+# BREED ------------------------
+# Basenji
+
+# CHARACTERISTICS ----------------
+# Size: Medium
+# Type: Purebred
+# Life span: 10-12 years
+# Name: varies
+# Color: varies
+# Height: 15 - 17 inches
+# Weight: 20 - 26 pounds
+
+# BEHAVIOR -----------------------
+# Affectionate
+# Alert
+# Curious
+# Energetic
+# Playful
+# Intelligent
