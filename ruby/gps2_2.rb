daac44d9958c28ee95542ea38ddb8fe5955bc9e3
Vince Roti <vince.roti@gmail.com>
2016-09-07 14:37:59 -0500

    update psuedocode

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -6,28 +6,29 @@
   # put items into array and SPLIT them
   # have empty hash to SHOVEL items into 
   # set default amount to 1
-  # print the list to the console [can you use one of your other methods here?]
+  # print the list to the console 
 # output: display hash with descriptive(carrots, apples, etc) items/keys that show the quantity of each item
 
 # Method to add an item to a list
-# input: item name and optional quantity which takes data from hash 
-# steps: 
-# output:
+# input: item name and optional quantity which takes data from hash
+# steps: define a method and it takes in grocery_hash
+#        allow for entry of new items and quantity
+# output: show updated grocery_hash
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: grocery_hash from list_creator method
+# steps: remove KEY from grocery_hash using iteration
+# output: show updated grocery_hash
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: grocery_hash from list_creator method and what 
+# steps: add or remove quantity of KEY from grocery_hash using iteration
+# output: show updated grocery_hash
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: takes in grocery_hash
+# steps: iterate through grocery_hash and use INTERPERLATION to show the KEY and VALUE in pretty way
+# output: shows step above
 
 list = "apples oranges grapes apples milk paper"
 
