2e8fb5019f5fa24d1a6da356d5c34b92d5437c17
Vince Roti <vince.roti@gmail.com>
2016-09-08 19:59:09 -0500

    shout.rb release 1 completion

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,7 +1,16 @@
 #Shout Module Creation
-
+#Release 1
 module Shout
-  def self.yell_angrily(words)
+  def self.yell_angrily(words) #the methods of standalone modules must be defined on the self keyword
     words + "!!!" + " :("
   end
+
+  def self.yell_happily(words)
+    words + "!!!" + " :)"
+  end
+
+  self.yell_happily #correct deceleration in order to call module in IRB - String can be set to whatever in IRB or in file
+
+  self.yell_angrily("RAWR") #correct deceleration in order to call module in IRB
+
 end
