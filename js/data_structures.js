7ffbe79e3f9b7775effb7601c8e5626ac8516462
Vince Roti <vince.roti@gmail.com>
2016-09-14 15:39:03 -0500

    release 2, created car constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -9,5 +9,27 @@ for (var i = 0; i < colors.length; i++) {
   horses[names[i]]=colors[i];
 }
 
-
 console.log(horses);
+
+var car = {
+  year: 2005,
+  color: "red",
+  make: "Audi"
+};
+
+function Car(year, color, make) {
+  this.year = year;
+  this.color = color;
+  this.make = make;
+
+  this.maxSpeed = function() { console.log("You are going max speed!!!"); };
+  console.log("Car Initiation Complete!!");
+}
+
+var first_car =  new Car(2010, "Black", "Lambo");
+
+first_car.maxSpeed();
+
+var second_car = new Car(2016, "Blue", "Toyota");
+
+console.log(second_car.color);
