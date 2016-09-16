1b7fbcea4f4ce74fcdd38f932971ee486d61a181
Vince Roti <vince.roti@gmail.com>
2016-09-15 20:07:39 -0500

    release 1 complete

--- js/algos.js
+++ js/algos.js
@@ -1,8 +1,18 @@
 // Pseudocode
 
+// Release 1
+
 // Using conditional statements use the lengths of each array index to determine which
 // to print based on which is the longest
 // Make a loop that goes through an array and prints the variable created in the condition
+
+// Release 2
+
+// Same thing as release one but different syntax. Use another for loop that will go through
+// one object and compare it to the other
+// using how to loop through objects in last assignment will help
+// If the objects have at least one KEY value they share, return true
+// If no values are shared return false
 
 function Length_Finder(array){
   array = array; // sets the parameter to array
@@ -16,8 +26,29 @@ function Length_Finder(array){
   console.log(longest_word); // prints longest word
 }
 
-var array_one = new Length_Finder(["long phrase","longest phrase","longer phrase","This one is even longer then that one"]);
+function Key_Value(object1, object2){
+  object1 = object1; // sets parameter for first object
+  object2 = object2; // sets parameter for second object
 
-var array_two = new Length_Finder(["Vince","Will","Thomas The Tank"]);
 
-var array_three = new Length_Finder(["Chicago", "San Francisco", "Texas"]);
+  for (keys in object1 && object2) { // loops through the keys in both objects
+    if (object1[keys] == object2[keys]) { // if the key in object one == object2
+     var return_value = true; // returns true if condition is met as sets it as variable
+
+    }
+    else if (return_value  == undefined && object1[keys] != object2[keys]) {
+      var return_value = false; //if first condition is not met return_value will not be made, thus undefined
+    }; // and if undefined and the keys don't equal each other at any point then return value as false
+  };
+  return return_value; // updates return value for function
+}
+
+
+// Driver Code
+Length_Finder(["long phrase","longest phrase","longer phrase","This one is even longer then that one"]);
+Length_Finder(["Vince","Will","Thomas The Tank"]);
+Length_Finder(["Chicago", "San Francisco", "Texas"]);
+
+console.log(Key_Value({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(Key_Value({name: "Steven", age: 44}, {name: "Tamir", age: 54}));
+console.log(Key_Value({name: "Will", age: 44, Language: "English"}, {name: "Vince", age: 23, Language: "English"}));
